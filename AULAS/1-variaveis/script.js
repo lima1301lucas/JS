89febd402fe465119b8795b8a4549ec40d0ee8f6
Lucas Lima <lima1301lucas@gmail.com>
2024-04-08 17:07:32 -0300

    Aulas 1, 2 e 3

---
+++ AULAS/1-variaveis/script.js
@@ -0,0 +1,21 @@
+// Declarar uma variável com o seu nome
+var nome = 'Lucas';
+
+// Declarar uma variável com a sua idade
+var idade = 21;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Pizza de frango com Catupiry';
+
+// Declarar 5 variáveis diferentes sem valores
+var time = 'Nenhum',
+  professor,
+  pais,
+  endereco,
+  rua;
+
+console.log(nome, idade, comidaFavorita);
---
+++ AULAS/2-tipos_de_dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nome = 'Lucas';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2003';
+
+// Declare uma variável com a sua idade
+var idade = 21;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Lima'
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
+
+console.log(verificarTipoNome);
+
+// apagar
